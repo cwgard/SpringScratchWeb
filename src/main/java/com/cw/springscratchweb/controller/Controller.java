e963dd6ddd864d20922ae38d501ea2983161f904
cwgard <82245524+cwgard@users.noreply.github.com>
2023-04-21 20:14:50 -0400

    add /hello endpoint with Controller and Service

--- src/main/java/com/cw/springscratchweb/controller/Controller.java
+++ src/main/java/com/cw/springscratchweb/controller/Controller.java
@@ -1,7 +1,23 @@
 package com.cw.springscratchweb.controller;
 
+import com.cw.springscratchweb.service.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+
+    private final Service service;
+
+    public Controller(Service service) {
+        this.service = service;
+    }
+
+    @GetMapping(value = "/hello")
+    @ResponseBody
+    public String hello() {
+        return service.hello();
+    }
+
 }
