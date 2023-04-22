c2b071366efbee158d8ae0130ee9aeb9970c7987
cwgard <82245524+cwgard@users.noreply.github.com>
2023-04-21 20:04:43 -0400

    add contextLoads unit test

--- src/test/java/com/cw/springscratchweb/SpringScratchWebApplicationTests.java
+++ src/test/java/com/cw/springscratchweb/SpringScratchWebApplicationTests.java
@@ -1,13 +1,26 @@
 package com.cw.springscratchweb;
 
+import com.cw.springscratchweb.controller.Controller;
+import com.cw.springscratchweb.service.Service;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class SpringScratchWebApplicationTests {
 
+	@Autowired
+	private Controller controller;
+
+	@MockBean
+	private Service service;
+
 	@Test
 	void contextLoads() {
+		System.out.println(controller.getClass());
+		System.out.println(controller.toString());
+		System.out.println(service.getClass());
+		System.out.println(service.toString());
 	}
-
 }
