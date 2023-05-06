edcd53acdd89d8f6be0fe6c39285ce8516420aa4
cwgard <82245524+cwgard@users.noreply.github.com>
2023-05-06 10:34:10 -0400

    add Config.java, //TODO

---
+++ src/main/java/com/cw/springscratchweb/unsurewheretoputthis/Config.java
@@ -0,0 +1,12 @@
+package com.cw.springscratchweb.unsurewheretoputthis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.logging.Logger;
+
+@Configuration
+public class Config {
+    //TODO Add spring configuration.
+
+}
