2aaa75d0c6d385382d0155308dee35fd449ebf66
cwgard <82245524+cwgard@users.noreply.github.com>
2023-05-06 10:42:24 -0400

    clean Config

--- src/main/java/com/cw/springscratchweb/unsurewheretoputthis/Config.java
+++ src/main/java/com/cw/springscratchweb/unsurewheretoputthis/Config.java
@@ -1,12 +1,8 @@
 package com.cw.springscratchweb.unsurewheretoputthis;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.logging.Logger;
 
 @Configuration
 public class Config {
     //TODO Add spring configuration.
-
 }
